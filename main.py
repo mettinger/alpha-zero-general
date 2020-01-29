1bdf2376a78468da8f5fe4f6e277e9c37ff3f6e8
mettinger <jettinger35@gmail.com>
2020-01-29 13:43:40 -0500

    message

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ else:
     checkpoint = '''/content/drive/My Drive/models/'''
     
 args = dotdict({
-    'numIters': 100,              # Number of self-play and model fit rounds.
+    'numIters': 10,              # Number of self-play and model fit rounds.
     'numEps': 100,               # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
-    'updateThreshold': 0.55,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'updateThreshold': 0.45,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
     'numMCTSSims': 50,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
+    'arenaCompare': 50,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
     'checkpoint': checkpoint,
@@ -46,9 +46,9 @@ if __name__ == "__main__":
         g = TicTacToeGame()
     elif gameChoice == 2:
         config = {'maxPileSize':10, 
-                'maxNumPile':10, 
-                'initialState':np.array([1 for i in range(10)]), 
-                'randomInitial':False}
+                  'maxNumPile':10, 
+                  'initialState':np.array([1 for i in range(10)]), 
+                  'randomInitial':False}
 
         g = nimGame(config)
 
